5b17299ab2f214e706012b3b27e418440a0e2302
Prem <yprem0092@gmail.com>
2023-07-22 13:46:24 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1 = input("enter num1: ")
+num2 = input("enter num2: ")
+opr = input("Enter the opr...: ")
+if opr == "+":
+    print(num1 + num2)
+elif opr == "-":
+    print(num1 - num2)
+elif opr == "*":
+    print(num1 * num2)
+elif opr == "/":
+    print(num1 / num2)
+else:
+    print('invalid opr...')
---
+++ deskfile.py
@@ -0,0 +1,2 @@
+desk = "orem@prem-vivobook-ASUSLaptop-X421EAYB-K413EA/python/"
+f = open(desk + "prem.txt", "w")
